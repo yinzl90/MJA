f7e23212584ec5ccf64be468033c19257313dcde
Zhengliang Yin <yinzhengliang@gmail.com>
2023-12-27 12:58:58 +0800

    rm build
    rm build

--- build/CMakeFiles/MJA_Qt.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for MJA_Qt.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/MJA_Qt_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/MJA_Qt_autogen"
-  "MJA_Qt_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/MJA_Qt_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()

deba946e3a25090f1f0c61a95e9e938dcbbdc190
Zhengliang Yin <yinzhengliang@gmail.com>
2023-12-28 20:34:34 +0800

    copilot sug
    copilot sug

--- codes/Game.cpp
+++ codes/Game.cpp
@@ -2,24 +2,18 @@
 
 #include <random>
 #include <algorithm>
+#include <array>
 
 Game::Game() {
+    array<MJType, 3> types = {MJType::Tuo, MJType::Suo, MJType::Wan};
     for (int i = 1; i <= 9; i++) {
-        deck.push_back(Stone(i, MJType::Wan));
-        deck.push_back(Stone(i, MJType::Wan));
-        deck.push_back(Stone(i, MJType::Wan));
-        deck.push_back(Stone(i, MJType::Wan));
-
-        deck.push_back(Stone(i, MJType::Tuo));
-        deck.push_back(Stone(i, MJType::Tuo));
-        deck.push_back(Stone(i, MJType::Tuo));
-        deck.push_back(Stone(i, MJType::Tuo));
-
-        deck.push_back(Stone(i, MJType::Suo));
-        deck.push_back(Stone(i, MJType::Suo));
-        deck.push_back(Stone(i, MJType::Suo));
-        deck.push_back(Stone(i, MJType::Suo));
+        for (const auto& type : types) {
+            for (int j = 0; j < 4; j++) {
+                deck.push_back(Stone(i, type));
+            }
+        }
     }
+    // shuffle deck
     random_device rd;
     mt19937 rng(rd());
     shuffle(deck.begin(), deck.end(), rng);
@@ -32,22 +26,15 @@ void Game::endGame() {
 void Game::restart() {
     for (Player p : players) {
     }
+    array<MJType, 3> types = {MJType::Tuo, MJType::Suo, MJType::Wan};
     for (int i = 1; i <= 9; i++) {
-        deck.push_back(Stone(i, MJType::Wan));
-        deck.push_back(Stone(i, MJType::Wan));
-        deck.push_back(Stone(i, MJType::Wan));
-        deck.push_back(Stone(i, MJType::Wan));
-
-        deck.push_back(Stone(i, MJType::Tuo));
-        deck.push_back(Stone(i, MJType::Tuo));
-        deck.push_back(Stone(i, MJType::Tuo));
-        deck.push_back(Stone(i, MJType::Tuo));
-
-        deck.push_back(Stone(i, MJType::Suo));
-        deck.push_back(Stone(i, MJType::Suo));
-        deck.push_back(Stone(i, MJType::Suo));
-        deck.push_back(Stone(i, MJType::Suo));
+        for (const auto& type : types) {
+            for (int j = 0; j < 4; j++) {
+                deck.push_back(Stone(i, type));
+            }
+        }
     }
+    // shuffle deck
     random_device rd;
     mt19937 rng(rd());
     shuffle(deck.begin(), deck.end(), rng);
